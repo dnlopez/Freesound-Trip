cf2fc2a2aa103472ec157506014791ae05598873
Daniel Lopez <daniel.lopez999@gmail.com>
2017-12-19 01:14:39 +0000

    Tidy FlyControls.js

--- FlyControls.js
+++ FlyControls.js
@@ -65,18 +65,21 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
     // false:
     //  mouselook is activated all the time,
     //  and left/right buttons cause forward/backward positional movement
+
     this.mouse_isDragging = false;
     // Only used if mouse_mustHoldButtonToLook is true
 
     // + Unused {{{
 
+    /*
     this.handleEvent = function (i_event)
     {
-        if (typeof this[i_event.type] == 'function')
+        if (typeof this[i_event.type] == "function")
         {
             this[i_event.type](i_event);
         }
     };
+    */
 
     // + }}}
 
@@ -84,7 +87,7 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
 
     // + + Mouse {{{
 
-    this.mousedown = function (i_event)
+    this.eventTarget_onMousedown = function (i_event)
     {
         if (i_event.button != 0)
             return;
@@ -119,32 +122,33 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
         i_event.preventDefault();
         i_event.stopPropagation();
     };
-    this.eventTarget.addEventListener('mousedown', bind(this, this.mousedown), false);
+    this.eventTarget.addEventListener("mousedown", bind(this, this.eventTarget_onMousedown), false);
 
-    this.mousemove = function (i_event)
+    this.eventTarget_onMousemove = function (i_event)
     {
         if (!this.mouse_mustHoldButtonToLook || this.mouse_isDragging)
         {
             // Set yaw and pitch according to mouse offset from the centre of the event target
             var eventTarget = this.getEventTargetDimensions();
             var halfWidth = eventTarget.size[0] / 2;
-            var halfHeight = eventTarget.size[1] / 2;
             this.inputState.yawLeft = -((i_event.pageX - eventTarget.offset[0]) - halfWidth) / halfWidth;
+            var halfHeight = eventTarget.size[1] / 2;
             this.inputState.pitchDown = ((i_event.pageY - eventTarget.offset[1]) - halfHeight) / halfHeight;
 
             this.updateRotationalVelocity();
         }
     };
-    this.eventTarget.addEventListener('mousemove', bind(this, this.mousemove), false);
+    this.eventTarget.addEventListener("mousemove", bind(this, this.eventTarget_onMousemove), false);
 
-    this.mouseup = function (i_event)
+    this.eventTarget_onMouseup = function (i_event)
     {
         if (this.mouse_mustHoldButtonToLook)
         {
             // Stop dragging and zero out yaw and pitch
             this.mouse_isDragging = false;
 
-            this.inputState.yawLeft = this.inputState.pitchDown = 0;
+            this.inputState.yawLeft = 0;
+            this.inputState.pitchDown = 0;
         }
         else
         {
@@ -167,7 +171,7 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
         i_event.preventDefault();
         i_event.stopPropagation();
     };
-    this.eventTarget.addEventListener('mouseup', bind(this, this.mouseup), false);
+    this.eventTarget.addEventListener("mouseup", bind(this, this.eventTarget_onMouseup), false);
 
     // + + }}}
 
@@ -180,50 +184,52 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
 
         switch (i_event.keyCode)
         {
-        case 16:
-            /* shift */ this.translationalSpeedMultiplier = 0.1;
+        /*
+        case 16:  // shift
+            this.translationalSpeedMultiplier = 0.1;
+            break;
+        */
+
+        case 87:  // W
+            this.inputState.forward = 1;
+            break;
+        case 83:  // S
+            this.inputState.back = 1;
             break;
 
-        case 87:
-            /*W*/ this.inputState.forward = 1;
+        case 65:  // A
+            this.inputState.left = 1;
             break;
-        case 83:
-            /*S*/ this.inputState.back = 1;
-            break;
-
-        case 65:
-            /*A*/ this.inputState.left = 1;
-            break;
-        case 68:
-            /*D*/ this.inputState.right = 1;
+        case 68:  // D
+            this.inputState.right = 1;
             break;
 
-        case 82:
-            /*R*/ this.inputState.up = 1;
+        case 82:  // R
+            this.inputState.up = 1;
             break;
-        case 70:
-            /*F*/ this.inputState.down = 1;
-            break;
-
-        case 38:
-            /*up*/ this.inputState.pitchUp = 1;
-            break;
-        case 40:
-            /*down*/ this.inputState.pitchDown = 1;
+        case 70:  // F
+            this.inputState.down = 1;
             break;
 
-        case 37:
-            /*left*/ this.inputState.yawLeft = 1;
+        case 38:  // up
+            this.inputState.pitchUp = 1;
             break;
-        case 39:
-            /*right*/ this.inputState.yawRight = 1;
+        case 40:  // down
+            this.inputState.pitchDown = 1;
             break;
 
-        case 81:
-            /*Q*/ this.inputState.rollLeft = 1;
+        case 37:  // left
+            this.inputState.yawLeft = 1;
             break;
-        case 69:
-            /*E*/ this.inputState.rollRight = 1;
+        case 39:  // right
+            this.inputState.yawRight = 1;
+            break;
+
+        case 81:  // Q
+            this.inputState.rollLeft = 1;
+            break;
+        case 69:  // E
+            this.inputState.rollRight = 1;
             break;
         }
 
@@ -232,63 +238,65 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
 
         //i_event.preventDefault();
     };
-    this.eventTarget.addEventListener('keydown', bind(this, this.keydown), false);
+    this.eventTarget.addEventListener("keydown", bind(this, this.keydown), false);
 
     this.keyup = function (i_event)
     {
         switch (i_event.keyCode)
         {
-        case 16:
-            /* shift */ this.translationalSpeedMultiplier = 1;
+        /*
+        case 16:  // shift 
+            this.translationalSpeedMultiplier = 1;
+            break;
+        */
+
+        case 87:  // W
+            this.inputState.forward = 0;
+            break;
+        case 83:  // S
+            this.inputState.back = 0;
             break;
 
-        case 87:
-            /*W*/ this.inputState.forward = 0;
+        case 65:  // A
+            this.inputState.left = 0;
             break;
-        case 83:
-            /*S*/ this.inputState.back = 0;
-            break;
-
-        case 65:
-            /*A*/ this.inputState.left = 0;
-            break;
-        case 68:
-            /*D*/ this.inputState.right = 0;
+        case 68:  // D
+            this.inputState.right = 0;
             break;
 
-        case 82:
-            /*R*/ this.inputState.up = 0;
+        case 82:  // R
+            this.inputState.up = 0;
             break;
-        case 70:
-            /*F*/ this.inputState.down = 0;
-            break;
-
-        case 38:
-            /*up*/ this.inputState.pitchUp = 0;
-            break;
-        case 40:
-            /*down*/ this.inputState.pitchDown = 0;
+        case 70:  // F
+            this.inputState.down = 0;
             break;
 
-        case 37:
-            /*left*/ this.inputState.yawLeft = 0;
+        case 38:  // up
+            this.inputState.pitchUp = 0;
             break;
-        case 39:
-            /*right*/ this.inputState.yawRight = 0;
+        case 40:  // down
+            this.inputState.pitchDown = 0;
             break;
 
-        case 81:
-            /*Q*/ this.inputState.rollLeft = 0;
+        case 37:  // left
+            this.inputState.yawLeft = 0;
             break;
-        case 69:
-            /*E*/ this.inputState.rollRight = 0;
+        case 39:  // right
+            this.inputState.yawRight = 0;
+            break;
+
+        case 81:  // Q
+            this.inputState.rollLeft = 0;
+            break;
+        case 69:  // E
+            this.inputState.rollRight = 0;
             break;
         }
 
         this.updateTranslationalVelocity();
         this.updateRotationalVelocity();
     };
-    this.eventTarget.addEventListener('keyup', bind(this, this.keyup), false);
+    this.eventTarget.addEventListener("keyup", bind(this, this.keyup), false);
 
     // + + }}}
 
@@ -347,7 +355,7 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
         this.translationalVelocity.y = (-this.inputState.down + this.inputState.up);
         this.translationalVelocity.z = (-this.inputState.forward + this.inputState.back);
 
-        //console.log('move:', [this.translationalVelocity.x, this.translationalVelocity.y, this.translationalVelocity.z]);
+        //console.log("move:", [this.translationalVelocity.x, this.translationalVelocity.y, this.translationalVelocity.z]);
     };
     this.updateTranslationalVelocity();
 
@@ -359,7 +367,7 @@ var THREE_FlyControls = function (i_object, i_eventTarget)
         this.rotationalVelocity.y = (-this.inputState.yawRight + this.inputState.yawLeft);
         this.rotationalVelocity.z = (-this.inputState.rollRight + this.inputState.rollLeft);
 
-        //console.log('rotate:', [this.rotationalVelocity.x, this.rotationalVelocity.y, this.rotationalVelocity.z]);
+        //console.log("rotate:", [this.rotationalVelocity.x, this.rotationalVelocity.y, this.rotationalVelocity.z]);
     };
     this.updateRotationalVelocity();
 
